df06a2bcb110928876b25946d447ef697bedb385
Dani <daniamir2001@yahoo.com>
2022-02-16 13:34:04 -0600

    Fixed styles.css bug

--- main.js
+++ main.js
@@ -1,5 +1,4 @@
-import './style.css'
-import * as THREE from '/node_modules/three/build/three.js';
+import * as THREE from '/node_modules/three/build/three.module.js';
 import {OrbitControls} from '/node_modules/three/examples/jsm/controls/OrbitControls.js';
 
 // Code for collapse elements
